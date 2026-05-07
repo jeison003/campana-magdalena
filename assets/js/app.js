900ad6d3ab20581613041216263a4b6f20716f15
jeison003 <jeisonmartinezev@unimagdalena.edu.co>
2026-05-07 15:46:11 -0500

    v5.2 feature: Se aplico efectos de animacion de transicion en todas las paginas x2

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,14 @@ import { markActiveNav } from "./router.js";
 import { initMobileMenu } from "./components/nav.js";
 import { showToast } from "./components/toast.js";
 import { initAccordion } from "./components/accordion.js";
+import { initScrollAnimations } from "./components/animations.js";
 import { siteConfig } from "./data.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   markActiveNav();
   initMobileMenu();
   initAccordion();
+  initScrollAnimations();
 
   const demoSubmitButton = document.getElementById("demoSubmitButton");
 
